2f5e2666e1d007b76cec9dea69709c1a532793b3
P1nero <1070402601@qq.com>
2024-07-03 13:17:15 +0800

    添加按键绑定和做纯客户端处理

--- src/client/java/net/p1nero/kimi/KimiClient.java
+++ src/client/java/net/p1nero/kimi/KimiClient.java
@@ -8,9 +8,14 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,11 +33,26 @@ public class KimiClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("minekimi");
 	public static final String MOD_ID = "minekimi";
 	private static final ArrayList<JsonObject> HISTORIES = new ArrayList<>();
+	private static KeyBinding openKimi;
 
 	@Override
 	public void onInitializeClient() {
 
 		Config.onInitialize();
+
+		openKimi = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.minekimi.open",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_K,
+				"category.minekimi.open"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (openKimi.wasPressed()) {
+				client.setScreen(new ChatScreen("/kimi "));
+			}
+		});
+
 		System.out.println("hello");
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(literal("kimi")
 				.then(argument("message", StringArgumentType.greedyString())
